7cf8da3f89645f295c7e7617cf49ebfdf77af07f
Jared Robinson <jarobinson.121@gmail.com>
2023-09-13 11:38:45 -0600

    Fixing the poor save

--- Unit01-Intro-to-cpp/homework1b-file-io/reformat.cpp
+++ Unit01-Intro-to-cpp/homework1b-file-io/reformat.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
 
 int main(int argc, char const* argv[]) {
-    // Write your code here
+    
+    ifstream inputFile(argv[1]);
+    ofstream outputFile(argv[2]);
+
+    if(inputFile.is_open()) {
+        cout << "The File WorkS!!!";
+    }
+    
+    else {
+        cout << "booty";
+    }
+
+    if(outputFile.is_open()) {
+        cout << "The File WorkS!!!2";
+    }
+    
+    else {
+        cout << "booty2";
+    }
+
+    inputFile.close();
+
+    outputFile.close();
 
     return 0;
 }
