7c23a271a86a83699366a82877ae19d1916504bf
Jared Robinson <jarobinson.121@gmail.com>
2023-09-18 22:42:43 -0600

    update(notes) Pushing changes from class

--- Unit01-Intro-to-cpp/homework1b-file-io/longest_line.cpp
+++ Unit01-Intro-to-cpp/homework1b-file-io/longest_line.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <fstream>
+
+using namespace std;
 
 int main(int argc, char const* argv[]) {
-    // Write your code here
+
+    ifstream inputFile(argv[1]);
+    int counter = 0;
+    string words;
+    istringstream iss(words);
+
+
+    if(!inputFile.is_open()) {
+        cout << "inputFile could not be opened.";
+    }
+
+    while(iss >> words) {
+        counter ++;
+    }
 
     return 0;
 }
--- Unit01-Intro-to-cpp/homework1b-file-io/reformat.cpp
+++ Unit01-Intro-to-cpp/homework1b-file-io/reformat.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <sstream>
 #include <iomanip>
 
 using namespace std;
@@ -21,46 +22,68 @@ int main(int argc, char const* argv[]) {
     }
 
     // Variable declarations
-    string delimiter = " ";
+    // string delimiter = " ";
     string input;
-    size_t spot = 0;
+    // size_t spot = 0;
     string firstName;
     string lastName;
     int points;
     double factor;
-    double newPoints;
-    string finalPoints;
-    string finalString;
+    // double newPoints;
+    // string finalPoints;
+    // string finalString;
 
-    while(getline(inputFile, input)) {
-        firstName = input.substr(0, input.find(delimiter));
+    while(!inputFile.eof()) {
 
-        input.erase(0, input.find(delimiter) + delimiter.length());
+        getline(inputFile, input);
 
-        lastName = input.substr(0, input.find(delimiter));
-        
-        input.erase(0, input.find(delimiter) + delimiter.length());
-    
-        points = stoi(input.substr(0, input.find(delimiter)));
-        
-        input.erase(0, input.find(delimiter) + delimiter.length());
+        istringstream iss(input);
 
-        factor = stod(input.substr(0, input.find(delimiter)));
-        
-        input.erase(0, input.find(delimiter) + delimiter.length());
+        iss >> firstName;
 
-        cout << setprecision(2);
+        iss >> lastName;
 
-        newPoints = points * factor;
+        iss >> points;
 
-        finalPoints = to_string(newPoints);
+        iss >> factor;
 
-        finalString = lastName + ", " + firstName + ": " + finalPoints;
-
-        outputFile << finalString << endl;
+        outputFile << lastName << ", " << firstName << ": " << (points * factor) << endl;
 
     }
+
+    // Attempt Number 1, help from Kyle
+    // while(getline(inputFile, input)) {
+    //     firstName = input.substr(0, input.find(delimiter));
+
+    //     input.erase(0, input.find(delimiter) + delimiter.length());
+
+    //     lastName = input.substr(0, input.find(delimiter));
+        
+    //     input.erase(0, input.find(delimiter) + delimiter.length());
     
+    //     points = stoi(input.substr(0, input.find(delimiter)));
+        
+    //     input.erase(0, input.find(delimiter) + delimiter.length());
+
+    //     factor = stod(input.substr(0, input.find(delimiter)));
+        
+    //     input.erase(0, input.find(delimiter) + delimiter.length());
+
+    //     cout << setprecision(2);
+
+    //     newPoints = points * factor;
+
+    //     finalPoints = to_string(newPoints);
+
+    //     if(finalPoints.find(".") != string::npos) {
+            
+    //     }
+
+    //     finalString = lastName + ", " + firstName + ": " + finalPoints;
+
+    //     outputFile << finalString << endl;
+
+    // }
 
     inputFile.close();
 
