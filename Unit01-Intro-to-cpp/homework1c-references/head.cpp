260e99809e4573e064dbf88aaf895056bd850f8d
Jared Robinson <jarobinson.121@gmail.com>
2023-09-20 21:58:41 -0600

    updates(notes) opened file and added checker

--- Unit01-Intro-to-cpp/homework1c-references/head.cpp
+++ Unit01-Intro-to-cpp/homework1c-references/head.cpp
@@ -1,30 +1,43 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
 int main(int argc, char const* argv[]) {
 
-    if(argc < 1) {
-        string s = argv[1];
-        cout << s << endl;
-    }
-
     int numLines = 10;
     string fnombre = "iGuessthisisafilename.ohboy";
+    string input;
 
-    for(int i = 1; i < argc; i++) {
-        if(argv[i][0] == '-') {
-            //this is specifying numLines
-            string lineStr = argv[i];
-            numLines = stoi(lineStr.substr(1));
+    if(argc > 1) {
+        for(int i = 1; i < argc; i++) {
+            if(argv[i][0] == '-') {
+                //this is specifying numLines
+                string lineStr = argv[i];
+                numLines = stoi(lineStr.substr(1));
+            }
+
+            else {
+                fnombre = argv[1];
+            }
         }
-        else {
-            fnombre = argv[1];
+
+        ifstream inputFile(fnombre);
+
+        if(!inputFile.is_open()) {
+            cerr << "ERROR: can't open " << fnombre;
         }
+
+
+
     }
 
-    
+    else {
+        while(getline(cin, input)) {
+            cout << input << endl;
+        }
+    }
 
     return 0;
 }
