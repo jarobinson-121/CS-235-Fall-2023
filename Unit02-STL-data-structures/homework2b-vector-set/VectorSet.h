b78b9f28cbfc9a65be8ef731da8bae9426a51073
Jared Robinson <jarobinson.121@gmail.com>
2023-10-11 10:26:13 -0600

    Completed Vector and Priority Queue labs

--- Unit02-STL-data-structures/homework2b-vector-set/VectorSet.h
+++ Unit02-STL-data-structures/homework2b-vector-set/VectorSet.h
@@ -1,42 +1,64 @@
 #pragma once
+#include <algorithm>
+#include <vector>
+ 
+using namespace std;
 
 template<class T>
 class VectorSet {
+private: 
+    vector<T> temp;
 public:
     bool contains(T item) const {
-        // implement contains here
-
-        // return true if item is in the set and false if not
+        for(unsigned int i = 0; i < temp.size(); i++) {
+            if(temp[i] == item) {
+                return true;
+            }
+        }
+        return false;
     }
 
     bool insert(T item) {
-        // implement insert here
-
-        // return true if item is inserted and false if item is already in the
-        // set
+        for(unsigned int i = 0; i < temp.size(); i++) {
+            if(temp[i] == item) {
+                return false;
+            }
+        }
+        
+        temp.push_back(item);
+        if(temp.back() == item) {
+            return true;
+        }
+        else {
+            return false;
+        }
+            
     }
 
     bool remove(T item) {
-        // implement remove here
-
-        // return true if item is removed and false if item wasn't in the set
+       for(unsigned int i = 0; i < temp.size(); i++) {
+            if(temp[i] == item) {
+                temp.erase(temp.begin() + i);
+                return true;
+            }
+        }
+        return false;
     }
 
     int size() const {
-        // implement size here
-
-        // return the number of items in the set
+        return temp.size();
     }
 
     bool empty() const {
-        // implement empty here
-
-        // return true if the set is empty and return false otherwise
+        if(temp.empty()) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     void clear() {
-        // implement clear here
-
-        // remove all items from the set
+        temp.clear();
     }
 };
--- Unit02-STL-data-structures/homework2c-pqueues/pqueues.cpp
+++ Unit02-STL-data-structures/homework2c-pqueues/pqueues.cpp
@@ -1,10 +1,55 @@
 #include <iostream>
 #include <string>
+#include <queue>
 
 #include "input.h"
 
+using namespace std;
+
 int main(int argc, char const* argv[]) {
-    // Write your code here
+    priority_queue<string> priQueue;
+    string response;
+    string prompt = "What do you want to do? ";
+    string name;
+    string newString;
+    int priority = 0;
+
+    bool check = input(prompt, response);
+
+    while(check == true) {
+        if(response == "add") {
+            cout << "Name: ";
+            getline(cin, name);
+
+            cout << "Priority: ";
+            cin >> priority;
+
+            if(priority < 10) {
+                newString = "0" + to_string(priority) + " - " + name;
+                priQueue.push(newString);
+            }
+            else {
+                newString = to_string(priority) + " - " + name;
+                priQueue.push(newString);
+            }
+            cin.ignore();
+        }
+        else if(response == "take") {
+            if(priQueue.empty()) {
+                cout << "There are no more people in line" << endl;
+            }
+            else {
+                cout << priQueue.top() << endl;
+                priQueue.pop();
+
+                cout.flush();
+            }
+        }
+        else {
+            cout << response << " isn't a valid operation" << endl;
+        }
+        check = input(prompt, response);
+    }
 
     return 0;
 }
